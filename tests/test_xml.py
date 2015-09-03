5c18b3016fb9f86a6a6762ff8311f6b426b9cd7f
Johan Ponin <johan.ponin.pro@gmail.com>
2015-09-03 17:53:05 +0200

    Added a simple test with non too small xml file.

---
+++ tests/test_xml.py
@@ -0,0 +1,23 @@
+from nose.tools import assert_equal
+
+from grammarize.xml import NaryGree
+
+from sax.saxg import root
+from sax.parserg import pp, xml as xmlb
+
+
+def test0():
+    t = xmlb(root(open('./samples/cv.settings.xml', 'rb')))
+    pp(t)
+    ng = NaryGree(t)
+    print(ng.bnf())
+    r = ng.rules()
+    e = {'office:settings': {'config:config-item-set'},
+         'office:document-settings': {'office:settings'},
+         'config:config-item-map-indexed': {'config:config-item-map-entry'},
+         'Document': {'office:document-settings'},
+         'config:config-item-set': {'config:config-item-map-indexed',
+                                    'config:config-item'},
+         'config:config-item-map-entry': {'config:config-item'},
+         'config:config-item': set()}
+    assert_equal(r, e)
