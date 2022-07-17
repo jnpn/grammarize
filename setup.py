fb3bb121063e8f9b258e4aa80d219730feb8c5a4
Johan PONIN <johan.ponin.pro@gmail.com>
2022-07-17 14:09:29 +0200

    build(setup): bump version and add deps to setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'DESCRIPTION.rst')) as f:
 
 setup(
     name='grammarize',
-    version='0.0.1a1',
+    version='0.0.1a2',
     description='tree grammar (partial) inferer',
     long_description=long_description,
     url='http://ponin.johan.free.fr/',
@@ -18,12 +18,24 @@ setup(
     author_email='johan.ponin.pro@gmail.com',
     license='GPLv2',
     package_dir={'grammarize': 'grammarize'},
-    classifiers=[],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: BNF',
+    ],
     keywords='tree grammar xml bnf',
     packages={'grammarize': 'grammarize'},
-    install_requires=[],
+    dependency_links=[
+        'git+https://github.com/jnpn/sax.git',
+    ],
+    install_requires=[
+        'click'
+    ],
     extras_require={
-        'dev': ['check-manifest'],
+        'dev': ['check-manifest', 'isort'],
         'test': ['nosetest']
     },
     entry_points={
