267847501560882dac3b5bd6aec009c655f66c82
Johan PONIN <johan.ponin.pro@gmail.com>
2022-07-17 22:27:12 +0200

    test(grammarize): rewrote tests to use stdlib's unittest

--- tests/test_gree.py
+++ tests/test_gree.py
@@ -1,41 +1,70 @@
+import unittest
+
 from grammarize.grammarize import Gree
 
-t0 = Gree('body',
-          Gree('div'),
-          Gree('div'))
 
-t1 = Gree('body',
-          Gree('div'),
-          Gree('div', Gree('wat',
-                           Gree('duh'),
-                           Gree('eww'))))
+class TestGree(unittest.TestCase):
 
-t2 = Gree('body',
-          Gree('ldiv'),
-          Gree('rdiv',
-               Gree('lwat',
-                    Gree('lduh'),
-                    Gree('leww')),
-               Gree('rwat',
-                    Gree('rduh'),
-                    Gree('reww'))))
+    def setUp(self):
+        self.t0 = Gree("body",
+                       Gree("div"),
+                       Gree("div"))
 
-t3 = Gree('body',
-          Gree('pre'),
-          Gree('div',
-               Gree('p',
-                    Gree('h1'),
-                    Gree('span')),
-               Gree('p',
-                    Gree('h2'),
-                    Gree('a'))))
+        self.t1 = Gree("body",
+                       Gree("div"),
+                       Gree("div",
+                            Gree("wat",
+                                 Gree("duh"),
+                                 Gree("eww"))))
 
-g3 = Gree('body',
-          Gree('pre'),
-          Gree('div',
-               Gree('p',
-                    Gree('h1'),
-                    Gree('span')),
-               Gree('p',
-                    Gree('h2'),
-                    Gree('a'))))
+        self.t2 = Gree("body",
+                       Gree("ldiv"),
+                       Gree("rdiv",
+                            Gree("lwat",
+                                 Gree("lduh"),
+                                 Gree("leww")),
+                            Gree("rwat",
+                                 Gree("rduh"),
+                                 Gree("reww")),
+                            ),
+                       )
+
+        self.t3 = Gree("body",
+                       Gree("pre"),
+                       Gree("div",
+                            Gree("p",
+                                 Gree("h1"),
+                                 Gree("span")),
+                            Gree("p",
+                                 Gree("h2"),
+                                 Gree("a")),
+                            ),
+                       )
+
+        self.g3 = Gree("body",
+                       Gree("pre"),
+                       Gree("div",
+                            Gree("p",
+                                 Gree("h1"),
+                                 Gree("span")),
+                            Gree("p",
+                                 Gree("h2"),
+                                 Gree("a")),
+                            ),
+                       )
+
+    def test_0(self):
+        l = Gree("div")
+        r = Gree("div")
+        t = Gree("body", l, r)
+        value = list(self.t0.walk())
+        expected = [l, t, r]
+        self.assertEqual(value, expected)
+
+    def test_1(self):
+        l = Gree("div")
+        r = Gree("div")
+        t = Gree("html", l, r)
+        value = list(self.t0.walk())
+        expected = [l, t, r]
+        self.assertNotEqual(value, expected)
